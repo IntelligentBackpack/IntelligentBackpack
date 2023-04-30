164ebaef1fae52dc314c148125c4604d08b8f0c3
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-04-30 18:46:42 +0200

    build: added .gradle and .idea folder to gitignore

---
+++ release.config.js
@@ -0,0 +1,20 @@
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
+./gradlew publishPlugins -Pgradle.publish.key="$GRADLE_PUBLISH_KEY" -Pgradle.publish.secret="$GRADLE_PUBLISH_SECRET" -PsigningKey="$SIGNING_KEY" -PsigningPassword="$SIGNING_PASSWORD" || exit 2
+./gradlew publishTestPythonPublicationToMavenRepository -PsigningKey="$SIGNING_KEY" -PsigningPassword="$SIGNING_PASSWORD" -PmavenCentralPwd="$MAVEN_PASSWORD" || exit 3
+git push --force origin \${nextRelease.version} || exit 4
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+            { "path": "build/libs/*.jar" },
+        ]
+    }],
+    "@semantic-release/git",
+)
+config.branches = [ "main" ]
+module.exports = config
