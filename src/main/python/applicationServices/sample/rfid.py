7f81c67ba5d9bbcfa43e71cb89928cd253f199d8
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-06 11:22:23 +0200

    ci: added pipeline

--- src/main/python/applicationServices/sample/rfid.py
+++ src/main/python/applicationServices/sample/rfid.py
@@ -1,13 +1,13 @@
 from time import sleep
-import sys
 from mfrc522 import SimpleMFRC522
+import GPIO
 reader = SimpleMFRC522()
 
 try:
     while True:
         print("Hold a tag near the reader")
         id, text = reader.read()
-        print("ID: %s\nText: %s" % (id,text))
+        print("ID: %s\nText: %s" % (id, text))
         sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
