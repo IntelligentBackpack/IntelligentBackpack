4ae7fd195ffaf00cb4cb65bcc91224823d89b54b
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-12 11:00:17 +0200

    chore: fixed setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup, find_packages
 
-class GradleDistribution(Distribution, object):
-    def __init__(self, attrs):
-        attrs['name'] = os.getenv('PYGRADLE_PROJECT_NAME')
-        attrs['version'] = os.getenv('PYGRADLE_PROJECT_VERSION')
-
-setup(
-    distclass=GradleDistribution,
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    include_package_data=True,
-)
+setup(name='src', version='1.0', packages=find_packages())
