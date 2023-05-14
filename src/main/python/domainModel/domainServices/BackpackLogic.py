37b8a888600ffc285000b58513e17e77a8e4df08
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-14 18:36:07 +0200

    chore: fixed BackpackLogic service to find elements and correctly use the insertion policy

--- src/main/python/domainModel/domainServices/BackpackLogic.py
+++ src/main/python/domainModel/domainServices/BackpackLogic.py
@@ -1,21 +1,33 @@
 # use the policies to add/remove elements into the Backpack retrieved and update db (use cases)
 from ..dataAccess.RepositoryAbstractClass import RepositoryAB
 from ..policies.InsertNewObjectPolicy import CanInsertNewObject
+from ..valueObjects.Book import Book
+from ..entities.Backpack import Backpack
 
-
-class Backpack:
+class BackpackLogicService:
     def __init__(self, repository: RepositoryAB):
         if isinstance(repository, RepositoryAB):
             # exception
             pass
         self.repository = repository
+        self.backpack = Backpack()
 
-    def addElementIntoBackpack(self, value):
-        if CanInsertNewObject().check_validity(self.repository, value):
-            self.repository.addElementIntoRepository(value)
+    def manageElement(self, isbn, tag):
+        book = Book(isbn=isbn, tagId=tag)
+        if CanInsertNewObject().check_validity(self.repository, self.backpack, book):
+            self.addElementIntoBackpack(tag)
+        else:
+            self.removeElementIntoBackpack(tag)
+
+    def addElementIntoBackpack(self, value: Book):
+        self.repository.addElementIntoRepository(value)
 
     def removeElementIntoBackpack(self, value):
         self.repository.removeElementIntoRepository(value)
+
+    def findElementIntoBackpack(self, value):
+        element = self.repository.findElementIntoRepository(value)
+        return element
 
     def checkElementMissed(self):
         return
--- src/main/python/domainModel/policies/InsertNewObjectPolicy.py
+++ src/main/python/domainModel/policies/InsertNewObjectPolicy.py
@@ -1,13 +1,15 @@
 from .Policy import PolicyAB
 from ..dataAccess.RepositoryAbstractClass import RepositoryAB
-
+from ..entities.Backpack import Backpack
 
 class CanInsertNewObject(PolicyAB):
 
     def __init__(self):
         self = self
 
-    def check_validity(self, repository, value):
+    def check_validity(self, repository, backpack: Backpack, value):
+        if backpack.check_book(book):
+            return False
         if isinstance(repository, RepositoryAB):
             array = repository.findElementIntoRepository(value)
             if len(array) > 0:
