f14a73c8eef7b9cc78028e0b7f3a2c80eb1f331e
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-14 18:37:12 +0200

    feat: added new getAllTags to LocalRepository and minor things fixed

--- src/main/python/infrastructureServices/repositories/accessData/LocalRepository.py
+++ src/main/python/infrastructureServices/repositories/accessData/LocalRepository.py
@@ -1,11 +1,11 @@
 from tinydb import TinyDB, Query, where
-
+import json
 
 class LocalRepositoryImpl:
 
     def __init__(self, name):
         self.name = name
-        self.db = TinyDB(name + '.json')
+        self.db = TinyDB('../../' + name + '.json')
         self.Tags = Query()
 
     def addElement(self, value):
@@ -14,6 +14,10 @@ class LocalRepositoryImpl:
     def removeElement(self, value):
         self.db.remove(where('tag') == value)
 
+    def getAllTags(self):
+        records = self.db.search(self.Tags.tag.exists())
+        return list(map(lambda record : record["tag"], json.loads(json.dumps(records))))
+
     def findElement(self, value):
-        record = self.db.search(self.Tags.tag == value)
-        print(record)
+        record = self.db.search(where("tag") == value)
+        return record != []
