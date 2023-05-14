f3b08035f5a76cec5ca1b379bfa5b6e71012dbc3
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-14 18:39:05 +0200

    feat: added new startup sync between local and remote db

--- src/main/python/infrastructureServices/repositories/RepositoryGateway.py
+++ src/main/python/infrastructureServices/repositories/RepositoryGateway.py
@@ -1,23 +1,26 @@
 from .accessData.LocalRepository import LocalRepositoryImpl
+from .accessData.RemoteRepository import RemoteRepositoryImpl
 from ...domainModel.dataAccess.RepositoryAbstractClass import RepositoryAB
 Repository = RepositoryAB
 
-
 class RepositoryGatewayImpl(Repository):
-
-    def __init__(self):
+    #TODO fare una factory??
+    def __init__(self, url, queue):
         self.localRepo = LocalRepositoryImpl("db")
+        self.remoteRepo = RemoteRepositoryImpl(url, queue)
+        tags_to_sync = self.localRepo.getAllTags()
+        for tag in tags_to_sync:
+            self.remoteRepo.addElement(tag)
 
     def addElementIntoRepository(self, newTag):
         self.localRepo.addElement(newTag)
+        self.remoteRepo.addElement(newTag)
 
     def removeElementIntoRepository(self, tag):
         self.localRepo.removeElement(tag)
+        self.remoteRepo.removeElement(tag)
 
     def findElementIntoRepository(self, tag):
-        self.localRepo.findElement(tag)
-
-# repo = RepositoryGateway()
-# repo.addElementIntoRepository("123456789")
-# repo.removeElementIntoRepository("123456789")
-# repo.findElementIntoRepository("123456789")
+        localPresence = self.localRepo.findElement(tag)
+        remotePresence = self.remoteRepo.findElement(tag)
+        return localPresence or remotePresence
