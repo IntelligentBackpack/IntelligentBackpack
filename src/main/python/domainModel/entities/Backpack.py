1386661f9f2123bd51fdf19720a8b63a33a342e6
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-14 18:34:36 +0200

    feat: added new backpack entity, to store backpack state

---
+++ src/main/python/domainModel/entities/Backpack.py
@@ -0,0 +1,22 @@
+from ..valueObjects.Book import Book
+
+class Backpack:
+
+    def __init__(self):
+        self.user_email = ""
+        self.books_lastly_added = []
+
+    def set_user(self, email):
+        self.user_email = email
+        return
+
+    def add_book(self, book: Book):
+        self.books_lastly_added.append(book)
+        return
+
+    def remove_book(self, book: Book):
+        self.remove_book(book)
+        return
+
+    def check_book(self, book: Book):
+        return book in self.books_lastly_added
