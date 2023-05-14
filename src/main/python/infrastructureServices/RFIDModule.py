5c00f3b487abcb2709bf5844ea5647f875a011d7
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-14 18:40:42 +0200

    feat: adapted RFID module with a thread

--- src/main/python/infrastructureServices/RFIDModule.py
+++ src/main/python/infrastructureServices/RFIDModule.py
@@ -1,22 +1,32 @@
 from time import sleep
 from mfrc522 import SimpleMFRC522
 import GPIO
+from threading import Thread
 
 
-class RFIDReader:
-    def __init__(self, consumer):
+class RFIDReader(Thread):
+    def __init__(self, consumer, cv):
+        Thread.__init__(self)
         self.consumer = consumer
         self.reader = SimpleMFRC522()
+        self.cv = cv
 
-    def startReading(self):
+    def run(self):
         try:
-            while True:
-                print("Hold a tag near the reader")
-                id, text = self.reader.read()
-                print("ID: %s\nText: %s" % (id, text))
-                # execute the high order function
-                self.consumer(id)
-                sleep(5)
+            with self.cv:
+                self.consumer(6)
+                self.cv.wait()
+
+                while True:
+                    
+                    print("Hold a tag near the reader")
+                    id, text = self.reader.read()
+                    print("ID: %s\nText: %s" % (id, text))
+                    # execute the high order function
+                    self.consumer(id)
+                    self.cv.notifyAll()
+                    sleep(5)
+
         except KeyboardInterrupt:
             GPIO.cleanup()
             raise
