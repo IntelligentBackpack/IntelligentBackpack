eb82b6ec7a79993a8bdcf27183adf17079e53f79
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-24 14:58:01 +0200

    fix: fixed insertion policy array length check

--- src/main/python/domainModel/policies/InsertNewObjectPolicy.py
+++ src/main/python/domainModel/policies/InsertNewObjectPolicy.py
@@ -2,17 +2,18 @@ from .Policy import PolicyAB
 from ..dataAccess.RepositoryAbstractClass import RepositoryAB
 from ..entities.Backpack import Backpack
 
+
 class CanInsertNewObject(PolicyAB):
 
     def __init__(self):
         self = self
 
-    def check_validity(self, repository, backpack: Backpack, value):
+    def check_validity(self, repository, backpack: Backpack, book):
         if backpack.check_book(book):
             return False
         if isinstance(repository, RepositoryAB):
-            array = repository.findElementIntoRepository(value)
-            if len(array) > 0:
+            value = repository.find_element_into_repository(book)
+            if value:
                 return False
             else:
                 return True
