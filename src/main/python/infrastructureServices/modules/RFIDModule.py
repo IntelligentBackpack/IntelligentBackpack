6fca740da13aad4aae445c06cdb60b2cdee85833
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-06-08 11:36:32 +0200

    chore: added rfid preprocessing

--- src/main/python/infrastructureServices/modules/RFIDModule.py
+++ src/main/python/infrastructureServices/modules/RFIDModule.py
@@ -32,10 +32,12 @@ class RFIDReader(Thread):
                 print("Hold a tag near the reader")
                 id, text = self.reader.read()
                 print("ID: %s\nText: %s" % (id, text))
+                chars = list(id)
+                value_to_send = chars[:2] + ":" + chars[2:4] + ":" + chars[4:6] + ":" + chars[6:8] + ":" + chars[8:10] + ":" + chars[10:12]
                 # execute the high order function
                 message_to_send = {
                     "type": "TAG_READ",
-                    "payload": text
+                    "payload": value_to_send
                 }
                 self.queue_messages.put(message_to_send)
                 sleep(5)
